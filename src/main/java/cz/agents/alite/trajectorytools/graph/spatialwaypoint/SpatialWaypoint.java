69bc25ca7b3cc3016887cbc214e19dcb08387289
Jan Tozicka <tozicka@agents.fel.cvut.cz>
2012-06-26 10:54:30 +0200

    Warning fixed
    Generic parametr added

--- src/main/java/cz/agents/alite/trajectorytools/graph/spatialwaypoint/SpatialWaypoint.java
+++ src/main/java/cz/agents/alite/trajectorytools/graph/spatialwaypoint/SpatialWaypoint.java
@@ -2,7 +2,9 @@ package cz.agents.alite.trajectorytools.graph.spatialwaypoint;
 
 import cz.agents.alite.trajectorytools.util.Point;
 
-public class SpatialWaypoint extends Point implements Comparable{
+public class SpatialWaypoint extends Point implements Comparable<SpatialWaypoint> {
+    private static final long serialVersionUID = -9146741978852925425L;
+
     public final int order;
     public SpatialWaypoint(int order, double x, double y) {
         super(x,y,0.0);
@@ -10,16 +12,12 @@ public class SpatialWaypoint extends Point implements Comparable{
     }
 
     @Override
-    public int compareTo(Object arg0) {
-        if (arg0 instanceof SpatialWaypoint) {
-            return order - ((SpatialWaypoint)arg0).order;
-        }
-        return 0;
+    public int compareTo(SpatialWaypoint other) {
+        return order - other.order;
     }
 
     public String toString() {
         return "#" + order;
         //return "#" + order + " "+ super.toString();
     }   
-    
 }
