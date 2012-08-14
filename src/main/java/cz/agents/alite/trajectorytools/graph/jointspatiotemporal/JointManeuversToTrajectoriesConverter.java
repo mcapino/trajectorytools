07536dfec97917c55e9e589246deb46cd0b51104
Michal Cap <cap@agents.fel.cvut.cz>
2012-08-14 10:04:14 +0200

    Removed unused imports

--- src/main/java/cz/agents/alite/trajectorytools/graph/jointspatiotemporal/JointManeuversToTrajectoriesConverter.java
+++ src/main/java/cz/agents/alite/trajectorytools/graph/jointspatiotemporal/JointManeuversToTrajectoriesConverter.java
@@ -8,17 +8,14 @@ import org.jgrapht.GraphPath;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.GraphPathImpl;
 
-import cz.agents.alite.tactical.util.Converter;
 import cz.agents.alite.trajectorytools.graph.spatiotemporal.maneuvers.SpatioTemporalManeuver;
 import cz.agents.alite.trajectorytools.trajectory.SpatioTemporalManeuverTrajectory;
 import cz.agents.alite.trajectorytools.trajectory.Trajectory;
-import cz.agents.alite.trajectorytools.trajectorymetrics.TrajecotryDistanceMetricTest;
 import cz.agents.alite.trajectorytools.util.TimePoint;
 
 public class JointManeuversToTrajectoriesConverter {
 
     public JointManeuversToTrajectoriesConverter() {
-        // TODO Auto-generated constructor stub
     }
 
     public static Trajectory[] convert(GraphPath<JointState, JointManeuver> path) {
@@ -32,13 +29,13 @@ public class JointManeuversToTrajectoriesConverter {
 
         for (int i = 0; i < nAgents; i++) {
             // Processing agent i
-            
-        	Graph<TimePoint, SpatioTemporalManeuver> graph = new DefaultDirectedGraph<TimePoint, SpatioTemporalManeuver>(SpatioTemporalManeuver.class);
+
+            Graph<TimePoint, SpatioTemporalManeuver> graph = new DefaultDirectedGraph<TimePoint, SpatioTemporalManeuver>(SpatioTemporalManeuver.class);
             TimePoint startTimePoint = startJointState.get(i);
             List<SpatioTemporalManeuver> edges = new LinkedList<SpatioTemporalManeuver>();
             TimePoint endTimePoint = path.getEndVertex().get(i);
             double duration = 0.0;
-            
+
             JointState currentJointState = startJointState;
             graph.addVertex(currentJointState.get(i));
             for (JointManeuver jointManeuver : jointManeuvers) {
@@ -48,10 +45,10 @@ public class JointManeuversToTrajectoriesConverter {
                 edges.add(edge);
                 duration += edge.getDuration();
             }
-            
-            GraphPath<TimePoint, SpatioTemporalManeuver> singleGraphPath 
-            	= new GraphPathImpl<TimePoint, SpatioTemporalManeuver>(graph, startTimePoint, endTimePoint, edges, duration);
-            
+
+            GraphPath<TimePoint, SpatioTemporalManeuver> singleGraphPath
+                = new GraphPathImpl<TimePoint, SpatioTemporalManeuver>(graph, startTimePoint, endTimePoint, edges, duration);
+
             trajectories[i] = new SpatioTemporalManeuverTrajectory<TimePoint, SpatioTemporalManeuver>(singleGraphPath, duration);
         }
 
