60a95ba0cdfd95b107dfd37433abb7e53ff27923
Michal Cap <cap@agents.fel.cvut.cz>
2012-10-26 17:18:35 +0200

    Added Heuristic to FlatSpatialStraightLineDomain

--- src/main/java/cz/agents/alite/trajectorytools/graph/spatial/rrtstar/FlatSpatialStraightLineDomain.java
+++ src/main/java/cz/agents/alite/trajectorytools/graph/spatial/rrtstar/FlatSpatialStraightLineDomain.java
@@ -8,34 +8,41 @@ import cz.agents.alite.trajectorytools.util.SpatialPoint;
 
 public class FlatSpatialStraightLineDomain extends SpatialStraightLineDomain {
 
-	private double defaultZ;
-	
-	private static final int RETURN_TARGET_FREQ = 10; 
-	private int currentReturnTarget = RETURN_TARGET_FREQ;
+    private double defaultZ;
 
-	private SpatialPoint targetPoint; 
+    private static final int RETURN_TARGET_FREQ = 10;
+    private int currentReturnTarget = RETURN_TARGET_FREQ;
 
-	public FlatSpatialStraightLineDomain(BoxRegion bounds,
-			Collection<SpaceRegion> obstacles, SpaceRegion targetRegion, SpatialPoint targetPoint, double speed,
-			double defaultZ) {
-		super(bounds, obstacles, targetRegion, speed);
-		this.targetPoint = targetPoint;
-		this.defaultZ = defaultZ;
-	}
+    private SpatialPoint targetPoint;
+
+    public FlatSpatialStraightLineDomain(BoxRegion bounds,
+            Collection<SpaceRegion> obstacles, SpaceRegion targetRegion, SpatialPoint targetPoint, double speed,
+            double defaultZ) {
+        super(bounds, obstacles, targetRegion, speed);
+        this.targetPoint = targetPoint;
+        this.defaultZ = defaultZ;
+    }
 
     @Override
     public SpatialPoint sampleState() {
-        SpatialPoint point;    	
-    	if ((--currentReturnTarget) < 0) {
-    		currentReturnTarget = RETURN_TARGET_FREQ;
-    		point = targetPoint;
-    	} else {
-	        do {
-	            double x = bounds.getCorner1().x + (random.nextDouble() * (bounds.getCorner2().x - bounds.getCorner1().x));
-	            double y = bounds.getCorner1().y + (random.nextDouble() * (bounds.getCorner2().y - bounds.getCorner1().y));
-	            point = new SpatialPoint(x, y, defaultZ);
-	        } while (!isInFreeSpace(point));
-    	}
+        SpatialPoint point;
+        if ((--currentReturnTarget) < 0) {
+            currentReturnTarget = RETURN_TARGET_FREQ;
+            point = targetPoint;
+        } else {
+            do {
+                double x = bounds.getCorner1().x + (random.nextDouble() * (bounds.getCorner2().x - bounds.getCorner1().x));
+                double y = bounds.getCorner1().y + (random.nextDouble() * (bounds.getCorner2().y - bounds.getCorner1().y));
+                point = new SpatialPoint(x, y, defaultZ);
+            } while (!isInFreeSpace(point));
+        }
         return point;
     }
+
+    @Override
+    public double estimateCostToGo(SpatialPoint p1) {
+        return p1.distance(targetPoint);
+    }
+
+
 }
