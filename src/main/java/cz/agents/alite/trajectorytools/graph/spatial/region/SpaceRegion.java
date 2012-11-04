5cf68eea6ffb586a50bad52b07f300ac800ccfc7
Michal Cap <cap@agents.fel.cvut.cz>
2012-11-05 00:55:45 +0100

    Cleaned up

--- src/main/java/cz/agents/alite/trajectorytools/graph/spatial/region/SpaceRegion.java
+++ src/main/java/cz/agents/alite/trajectorytools/graph/spatial/region/SpaceRegion.java
@@ -2,9 +2,6 @@ package cz.agents.alite.trajectorytools.graph.spatial.region;
 
 import cz.agents.alite.trajectorytools.util.SpatialPoint;
 
-/**
- * An obstacle in 3D + time.
- */
 public interface SpaceRegion {
 	public boolean intersectsLine(SpatialPoint p1, SpatialPoint p2);
 	public boolean isInside(SpatialPoint p);	
