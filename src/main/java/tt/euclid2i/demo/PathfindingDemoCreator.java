e16b62dd5f96d3ff3b68198cfca1f27f6d320c22
Michal Cap <cap@agents.fel.cvut.cz>
2013-04-03 16:47:25 +0200

    Added a demo that illustrates the use of A-star planner and lazy grid in Euclidean 2-i

---
+++ src/main/java/tt/euclid2i/demo/PathfindingDemoCreator.java
@@ -0,0 +1,105 @@
+package tt.euclid2i.demo;
+
+import java.awt.Color;
+import java.util.LinkedList;
+
+import javax.vecmath.Point2d;
+
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.AStarShortestPath;
+
+import tt.euclid2i.Line;
+import tt.euclid2i.Point;
+import tt.euclid2i.discretization.LazyGrid;
+import tt.euclid2i.region.Rectangle;
+import tt.euclid2i.region.Region;
+import tt.euclid2i.vis.ProjectionTo2d;
+import tt.vis.GraphLayer;
+import tt.vis.GraphLayer.GraphProvider;
+import tt.vis.GraphPathLayer;
+import tt.vis.GraphPathLayer.PathProvider;
+import cz.agents.alite.creator.Creator;
+import cz.agents.alite.vis.VisManager;
+import cz.agents.alite.vis.VisManager.SceneParams;
+import cz.agents.alite.vis.layer.common.ColorLayer;
+import cz.agents.alite.vis.layer.common.VisInfoLayer;
+
+
+public class PathfindingDemoCreator implements Creator {
+
+    @Override
+    public void init(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void create() {
+
+        // Create discretization
+        final DirectedGraph<Point, Line> graph = new LazyGrid(new Point(0,0), new LinkedList<Region>(), new Rectangle(new Point(-50,-50), new Point(50,50)), 10);
+
+        // plan the shortest path
+        final AStarShortestPath<Point, Line> astar = new AStarShortestPath<Point, Line>(graph, new Point(0,0), new Point(30,40), new AStarShortestPath.Heuristic<Point>() {
+            @Override
+            public double getHeuristicEstimate(Point current, Point goal) {
+                return current.distance(goal);
+            }
+        });
+
+        initVisualization();
+
+        // graph
+        VisManager.registerLayer(GraphLayer.create(new GraphProvider<Point, Line>() {
+
+            @Override
+            public Graph<Point, Line> getGraph() {
+                return ((LazyGrid) graph).generateFullGraph();
+            }
+        }, new ProjectionTo2d(), Color.GRAY, Color.GRAY, 1, 4));
+
+
+
+        // draw the shortest path
+        VisManager.registerLayer(GraphPathLayer.create(new PathProvider<Point, Line>() {
+
+            @Override
+            public GraphPath<Point, Line> getPath() {
+                return astar.getPath();
+            }
+
+        },  new ProjectionTo2d(),Color.RED, Color.RED, 2, 4));
+
+
+    }
+
+    private void initVisualization() {
+        VisManager.setInitParam("Trajectory Tools Vis", 1024, 768, 200, 200);
+        VisManager.setSceneParam(new SceneParams(){
+
+            @Override
+            public Point2d getDefaultLookAt() {
+                return new Point2d(0,0);
+            }
+
+            @Override
+            public double getDefaultZoomFactor() {
+                return 5;
+            } } );
+        VisManager.init();
+
+        // background
+        VisManager.registerLayer(ColorLayer.create(Color.WHITE));
+
+        // Overlay
+        VisManager.registerLayer(VisInfoLayer.create());
+    }
+
+
+    public static void main(String[] args) {
+        new PathfindingDemoCreator().create();
+    }
+
+}
