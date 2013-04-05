71960e2ce21179a2920bd4252b2517bd59e6e1ab
Michal Cap <cap@agents.fel.cvut.cz>
2013-04-05 17:37:46 +0200

    Code review

--- src/main/java/org/jgrapht/alg/AStarShortestPathFibanacci.java
+++ src/main/java/org/jgrapht/alg/AStarShortestPathFibanacci.java
@@ -3,38 +3,41 @@ package org.jgrapht.alg;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.Graphs;
 import org.jgrapht.graph.GraphPathImpl;
 import org.jgrapht.traverse.ClosestFirstIterator;
 import org.jgrapht.traverse.HeuristicIterator;
-import org.jgrapht.util.GoalChecker;
+import org.jgrapht.util.Goal;
 import org.jgrapht.util.Heuristic;
 
 /**
  * An implementation of <a href="http://en.wikipedia.org/wiki/A*_search_algorithm">A* search algorithm</a>.
- *
- * @author Vojtech Letal <letalvoj@fel.cvut.cz>
  */
 public class AStarShortestPathFibanacci<V, E> {
 
     private Graph<V, E> graph;
     private Heuristic<V> heuristic;
     private V startVertex;
-    private GoalChecker<V> goalChecker;
+    private Goal<V> goal;
     private double radius;
-    //
     private GraphPath<V, E> path;
 
     public static <V, E> GraphPath<V, E> findPathBetween(Graph<V, E> graph,
-            Heuristic<V> heuristic, V startVertex, V endVertex) {
+            Heuristic<V> heuristic, V startVertex, final V endVertex) {
 
-        return findPathBetween(graph, heuristic, startVertex, new EndVertexChecker<V>(endVertex));
+        return findPathBetween(graph, heuristic, startVertex, new Goal<V>() {
+            @Override
+            public boolean isGoal(V current) {
+                return current.equals(endVertex);
+            }
+        });
     }
 
     public static <V, E> GraphPath<V, E> findPathBetween(Graph<V, E> graph, Heuristic<V> heuristic,
-            V startVertex, GoalChecker<V> goalChecker) {
+            V startVertex, Goal<V> goalChecker) {
 
         AStarShortestPathFibanacci<V, E> alg = new AStarShortestPathFibanacci<V, E>(graph,
                 heuristic, startVertex, goalChecker, Double.POSITIVE_INFINITY);
@@ -44,23 +47,23 @@ public class AStarShortestPathFibanacci<V, E> {
     }
 
     private AStarShortestPathFibanacci(Graph<V, E> graph, Heuristic<V> heuristic, V startVertex,
-            GoalChecker<V> goalChecker, double radius) {
+            Goal<V> goalChecker, double radius) {
         this.graph = graph;
         this.heuristic = heuristic;
         this.startVertex = startVertex;
-        this.goalChecker = goalChecker;
+        this.goal = goalChecker;
         this.radius = radius;
     }
 
     private void findPath() {
         HeuristicIterator<V, E> iter =
                 new HeuristicIterator<V, E>(graph, heuristic, startVertex, radius);
-        
+
         while (iter.hasNext()) {
             V vertex = iter.next();
 
-            if (goalChecker.isGoal(vertex)) {
-                path = createEdgeList(graph, iter, startVertex, vertex);
+            if (goal.isGoal(vertex)) {
+                path = reconstructGraphPath(graph, iter, startVertex, vertex);
                 return;
             }
         }
@@ -68,21 +71,21 @@ public class AStarShortestPathFibanacci<V, E> {
         path = null;
     }
 
-    private static <V, E> GraphPath<V, E> createEdgeList(Graph<V, E> graph,
+    private static <V, E> GraphPath<V, E> reconstructGraphPath(Graph<V, E> graph,
             ClosestFirstIterator<V, E> iter, V startVertex, V endVertex) {
 
         List<E> edgeList = new ArrayList<E>();
         V v = endVertex;
 
         double pathLength = 0;
-        
+
         while (true) {
             E edge = iter.getSpanningTreeEdge(v);
-            
+
             if (edge == null) {
                 break;
             }
-            
+
             edgeList.add(edge);
             pathLength += graph.getEdgeWeight(edge);
             v = Graphs.getOppositeVertex(graph, edge, v);
@@ -94,17 +97,4 @@ public class AStarShortestPathFibanacci<V, E> {
                 endVertex, edgeList, pathLength);
     }
 
-    private static class EndVertexChecker<V> implements GoalChecker<V> {
-
-        private V endVertex;
-
-        public EndVertexChecker(V endVertex) {
-            this.endVertex = endVertex;
-        }
-
-        @Override
-        public boolean isGoal(V current) {
-            return current.equals(endVertex);
-        }
-    }
 }
--- src/main/java/org/jgrapht/util/Heuristic.java
+++ src/main/java/org/jgrapht/util/Heuristic.java
@@ -1,10 +1,5 @@
 package org.jgrapht.util;
 
-/**
- *
- * @author Vojtech Letal <letalvoj@fel.cvut.cz>
- */
 public interface Heuristic<V> {
-
     double getCostToGoalEstimate(V current);
 }
