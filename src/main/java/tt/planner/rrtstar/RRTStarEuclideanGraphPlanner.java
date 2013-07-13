2b7e4eaf575e52d13dc4f133396a1fc64e128a2d
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-07-13 20:36:40 +0200

    Euclidean Graph RRTStar introduced

---
+++ src/main/java/tt/planner/rrtstar/RRTStarEuclideanGraphPlanner.java
@@ -0,0 +1,100 @@
+package tt.planner.rrtstar;
+
+import tt.planner.rrtstar.graph.GraphDomain;
+import tt.planner.rrtstar.graph.GraphPathEdge;
+
+import java.util.Collection;
+
+public class RRTStarEuclideanGraphPlanner<S, E> extends RRTStarEuclideanPlanner<S, GraphPathEdge<S, E>> {
+
+
+    public RRTStarEuclideanGraphPlanner(GraphDomain<S, E> domain, EuclideanCoordinatesProvider<S> euclideanProvider,
+                                        S initialState, double gamma, double eta) {
+        super(domain, euclideanProvider, initialState, gamma, eta);
+    }
+
+    public RRTStarEuclideanGraphPlanner(GraphDomain<S, E> domain, EuclideanCoordinatesProvider<S> euclideanProvider,
+                                        S initialState, double gamma) {
+        super(domain, euclideanProvider, initialState, gamma);
+    }
+
+    @Override
+    protected void rewire(Vertex<S, GraphPathEdge<S, E>> candidateParent, Collection<Vertex<S, GraphPathEdge<S, E>>> vertices) {
+        for (Vertex<S, GraphPathEdge<S, E>> nearVertex : vertices) {
+            if (nearVertex != candidateParent) {
+                rewire(candidateParent, nearVertex);
+            }
+        }
+    }
+
+    private void rewire(Vertex<S, GraphPathEdge<S, E>> candidateParent, Vertex<S, GraphPathEdge<S, E>> vertex) {
+        S candidateParentState = candidateParent.getState();
+        S vertexState = vertex.getState();
+
+        Extension<S, GraphPathEdge<S, E>> extension = domain.extendTo(candidateParentState, vertexState);
+
+        if (extension == null || !vertexState.equals(extension.target)) return;
+
+        double rewiredCostToRoot = candidateParent.getCostFromRoot() + extension.cost;
+        double costToRoot = vertex.getCostFromRoot();
+
+        if (extension.exact && rewiredCostToRoot < costToRoot) {
+            insertExtension(candidateParent, extension, vertex);
+            updateBranchCost(vertex);
+        }
+    }
+
+    @Override
+    protected BestParentSearchResult findBestParent(S randomSample, Collection<Vertex<S, GraphPathEdge<S, E>>> nearVertices) {
+        BestParentSearchResult result = null;
+        ExtensionCost minCost = ExtensionCost.minCostValue();
+
+        for (Vertex<S, GraphPathEdge<S, E>> vertex : nearVertices) {
+            S candidateState = vertex.getState();
+
+            ExtensionEstimate extensionEst = domain.estimateExtension(candidateState, randomSample);
+            if (isEstimateWorse(extensionEst, minCost)) continue;
+
+            Extension<S, GraphPathEdge<S, E>> extension = domain.extendTo(candidateState, randomSample);
+            if (isExtensionWorse(randomSample, extension)) continue;
+
+            ExtensionCost cost = new ExtensionCost(vertex.getCostFromRoot(), extension.cost);
+            if (cost.compareTo(minCost) < 0) {
+                minCost = cost;
+                result = new BestParentSearchResult(vertex, extension);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isExtensionWorse(S randomSample, Extension<S, GraphPathEdge<S, E>> extension) {
+        return extension == null || !extension.exact || !randomSample.equals(extension.target);
+    }
+
+    private boolean isEstimateWorse(ExtensionEstimate extensionEst, ExtensionCost minCost) {
+        return extensionEst != null && extensionEst.cost > minCost.overallCost;
+    }
+
+    protected static class ExtensionCost implements Comparable<ExtensionCost> {
+        final double costFromRoot;
+        final double overallCost;
+
+        public ExtensionCost(double costFromRoot, double extensionCost) {
+            this.costFromRoot = costFromRoot;
+            this.overallCost = costFromRoot + extensionCost;
+        }
+
+
+        public static ExtensionCost minCostValue() {
+            return new ExtensionCost(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+        }
+
+        @Override
+        public int compareTo(ExtensionCost that) {
+            //TODO comparing double
+            int comp1 = Double.compare(this.overallCost, that.overallCost);
+            return comp1 == 0 ? Double.compare(that.costFromRoot, this.costFromRoot) : comp1;
+        }
+    }
+}
