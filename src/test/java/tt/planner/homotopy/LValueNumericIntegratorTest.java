7123058ee7088707ace620dc88e238ce843f2be5
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-07-22 16:32:45 +0200

    constant introduced

--- src/test/java/tt/planner/homotopy/LValueNumericIntegratorTest.java
+++ src/test/java/tt/planner/homotopy/LValueNumericIntegratorTest.java
@@ -5,8 +5,11 @@ import org.jscience.mathematics.number.Complex;
 import java.util.List;
 
 public class LValueNumericIntegratorTest extends AbstractLValueIntegratorTest {
+
+    private static final int SAMPLES_PER_EDGE = 1000;
+
     @Override
     LValueIntegrator initializeIntegrator(List<Complex> qRoots, List<Complex> pRoots) {
-        return new LValueNumericIntegrator(qRoots, pRoots, 1000);
+        return new LValueNumericIntegrator(qRoots, pRoots, SAMPLES_PER_EDGE);
     }
 }
