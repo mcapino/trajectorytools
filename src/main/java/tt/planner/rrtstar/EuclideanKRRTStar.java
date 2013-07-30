0f21fefc2967bb32cb4595c4a6e68f65e8eac3b0
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-07-30 16:56:35 +0200

    code cleaned

--- src/main/java/tt/planner/rrtstar/EuclideanKRRTStar.java
+++ src/main/java/tt/planner/rrtstar/EuclideanKRRTStar.java
@@ -4,41 +4,51 @@ import tt.planner.rrtstar.domain.Domain;
 import tt.planner.rrtstar.util.Vertex;
 import tt.util.EuclideanCoordinatesProvider;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 
 public class EuclideanKRRTStar<S extends tt.euclid2i.Point, E> extends EuclideanRRTStar<S, E> {
 
-    protected int maxVertices;
+    protected int k;
 
     public EuclideanKRRTStar(Domain<S, E> domain, EuclideanCoordinatesProvider<S> euclideanProvider,
-                             S initialState, int maxVertices) {
-        super(domain, euclideanProvider, initialState, Double.POSITIVE_INFINITY);
-
-        this.maxVertices = maxVertices;
+                             S initialState, int k) {
+        super(domain, euclideanProvider, initialState, Double.NaN);
+        this.k = k;
     }
 
-    /**
-     * Returns maximum number of vertices which should be returned by getVerticesWithinRadius(...) function
-     */
     @Override
     public double getNearBallRadius() {
-        return maxVertices;
+        return Double.NaN;
+    }
+
+
+    @Override
+    protected Collection<Vertex<S, E>> getNearParentCandidates(S state) {
+        return getKNearestVertices(state, k);
     }
 
     @Override
-    protected List<Vertex<S, E>> getVerticesWithinRadius(S state, double maxVertices) {
+    protected Collection<Vertex<S, E>> getNearChildrenCandidates(S state) {
+        // gets k+1 neighbours because the very first is the state S itself
+        LinkedList<Vertex<S, E>> childrenCandidates = getKNearestVertices(state, k + 1);
+        removeStateIfFirst(state, childrenCandidates);
+
+        return childrenCandidates;
+    }
+
+    protected LinkedList<Vertex<S, E>> getKNearestVertices(S state, int k) {
         double[] key = euclideanProvider.getEuclideanCoordinates(state);
 
         Iterator<Vertex<S, E>> iterator =
                 knnKdTree.getNearestNeighborIterator(key, kdKeys.size(), distanceFunction);
-        List<Vertex<S, E>> list = new LinkedList<Vertex<S, E>>();
+        LinkedList<Vertex<S, E>> list = new LinkedList<Vertex<S, E>>();
 
         Vertex<S, E> vertex = null;
 //        int vertices = 0;
 
-        for (int i = 0; i < maxVertices; i++) {
+        for (int i = 0; i < k; i++) {
             if (iterator.hasNext()) {
                 vertex = iterator.next();
                 list.add(vertex);
