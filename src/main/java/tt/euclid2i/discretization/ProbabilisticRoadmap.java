f2a5cc6f54855148626f4dbc9a5aa534260f2ab8
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-08-07 11:00:38 +0200

    Implemented with KDTree to improve performance

--- src/main/java/tt/euclid2i/discretization/ProbabilisticRoadmap.java
+++ src/main/java/tt/euclid2i/discretization/ProbabilisticRoadmap.java
@@ -1,47 +1,84 @@
 package tt.euclid2i.discretization;
 
-import java.util.Collection;
-import java.util.Random;
-
+import ags.utils.dataStructures.KdTree;
+import ags.utils.dataStructures.NearestNeighborIterator;
+import ags.utils.dataStructures.SquareEuclideanDistanceFunction;
 import org.jgrapht.DummyEdgeFactory;
 import org.jgrapht.graph.DirectedWeightedMultigraph;
-
 import tt.euclid2i.Line;
 import tt.euclid2i.Point;
 import tt.euclid2i.Region;
 import tt.euclid2i.region.Rectangle;
 import tt.euclid2i.util.Util;
 
+import java.util.Collection;
+import java.util.Random;
 
-public class ProbabilisticRoadmap extends DirectedWeightedMultigraph<Point, Line>{
+
+public class ProbabilisticRoadmap extends DirectedWeightedMultigraph<Point, Line> {
+
     private static final long serialVersionUID = 7461735648599585309L;
 
-    public ProbabilisticRoadmap(int n, int radius, Point[] customPoints, Rectangle bounds, Collection<Region> obstacles, Random random) {
+    private int nVertices;
+    private int radius;
+    private Point[] customPoints;
+    private Rectangle bounds;
+    private Collection<Region> obstacles;
+    private Random random;
+
+    private KdTree<Point> knnTree;
+
+    public ProbabilisticRoadmap(int nVertices, int radius, Point[] customPoints, Rectangle bounds, Collection<Region> obstacles, Random random) {
         super(new DummyEdgeFactory<Point, Line>());
+        this.nVertices = nVertices;
+        this.radius = radius;
+        this.customPoints = customPoints;
+        this.bounds = bounds;
+        this.obstacles = obstacles;
+        this.random = random;
 
-        Point[] points = new Point[n+customPoints.length];
+        this.knnTree = new KdTree<Point>(2);
 
-        for (int i=0; i<n; i++) {
-            int x = random.nextInt(bounds.getCorner2().x);
-            int y = random.nextInt(bounds.getCorner2().y);
-            Point point = new Point(x,y);
-            addVertex(point);
-            points[i] = point;
-        }
+        generateVertices();
+        generateEdges();
+    }
 
-        for (int i=0; i<customPoints.length; i++) {
-            addVertex(customPoints[i]);
-            points[n+i] = customPoints[i];
-        }
+    private void generateEdges() {
+        for (Point point : vertexSet()) {
+            NearestNeighborIterator<Point> iterator = knnTree.getNearestNeighborIterator(key(point), vertexSet().size(), new SquareEuclideanDistanceFunction());
 
-        for (int i=0; i<points.length; i++) {
-            for (int j=0; j<points.length; j++) {
-                if (i != j && points[i].distance(points[j]) < radius && Util.isVisible(points[i], points[j], obstacles)) {
-                    Line edge = new Line(points[i], points[j]);
-                    addEdge(points[i], points[j], edge);
-                }
+            while (iterator.hasNext()) {
+                Point next = iterator.next();
+
+                if (point.equals(next))
+                    continue;
+
+                if (point.distance(next) > radius)
+                    break;
+
+                if (Util.isVisible(point, next, obstacles))
+                    addEdge(point, next, new Line(point, next));
             }
         }
+    }
+
+    private void generateVertices() {
+        for (int i = 0; i < nVertices; i++) {
+            int x = random.nextInt(bounds.getCorner2().x);
+            int y = random.nextInt(bounds.getCorner2().y);
+            Point point = new Point(x, y);
+            addVertex(point);
+            knnTree.addPoint(key(point), point);
+        }
+
+        for (int i = 0; i < customPoints.length; i++) {
+            knnTree.addPoint(key(customPoints[i]), customPoints[i]);
+            addVertex(customPoints[i]);
+        }
+    }
+
+    private static double[] key(Point point) {
+        return new double[]{point.getX(), point.getY()};
     }
 
     @Override
