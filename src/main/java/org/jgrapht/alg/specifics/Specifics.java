d31267564d92453a843d3ed9c3817f81195480a8
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-08-20 14:54:16 +0200

    bugfix - edgesof

--- src/main/java/org/jgrapht/alg/specifics/Specifics.java
+++ src/main/java/org/jgrapht/alg/specifics/Specifics.java
@@ -1,10 +1,11 @@
 package org.jgrapht.alg.specifics;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Iterator;
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Provides unified interface for operations that are different in directed
@@ -18,37 +19,39 @@ public abstract class Specifics<V, E> {
 
     public abstract Set<E> incomingEdgesOf(V vertex);
 
-    @Deprecated
     public Set<? extends E> edgesOf(V vertex) {
-        return outgoingEdgesOf(vertex);
+        Set<E> edges = new HashSet<E>();
+        edges.addAll(outgoingEdgesOf(vertex));
+        edges.addAll(incomingEdgesOf(vertex));
+        return edges;
     }
 
     public Set<V> predecessorVertexSet(V vertex) {
-        return iterateIntoSet(predecessorVertexIterator(vertex));
+        return toSet(predecessorVertexIterator(vertex));
     }
 
     public Set<V> succesorVertexSet(V vertex) {
-        return iterateIntoSet(succesorVertexIterator(vertex));
+        return toSet(succesorVertexIterator(vertex));
     }
 
-    private Set<V> iterateIntoSet(Iterator<V> iterator) {
+    private Set<V> toSet(Iterator<V> iterator) {
         Set<V> set = new HashSet<V>();
-        for (Iterator<V> it = iterator; it.hasNext();) {
-            V v = it.next();
+        while (iterator.hasNext()) {
+            V v = iterator.next();
             set.add(v);
         }
         return set;
     }
 
     public Iterator<V> predecessorVertexIterator(V vertex) {
-        return createOpositeVertexIterator(vertex, incomingEdgesOf(vertex));
+        return opositeVertexIterator(vertex, incomingEdgesOf(vertex));
     }
 
     public Iterator<V> succesorVertexIterator(V vertex) {
-        return createOpositeVertexIterator(vertex, outgoingEdgesOf(vertex));
+        return opositeVertexIterator(vertex, outgoingEdgesOf(vertex));
     }
 
-    private Iterator<V> createOpositeVertexIterator(final V vertex, Set<E> edges) {
+    private Iterator<V> opositeVertexIterator(final V vertex, Set<E> edges) {
         final Iterator<E> edgeIterator = edges.iterator();
 
         Iterator<V> verticeIterator = new Iterator<V>() {
