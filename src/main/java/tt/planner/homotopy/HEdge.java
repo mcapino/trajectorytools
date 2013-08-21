d1ef78bf030ed97f4291c085be4c7911443ca41a
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-08-21 19:10:49 +0200

    hash code & equals implemented

--- src/main/java/tt/planner/homotopy/HEdge.java
+++ src/main/java/tt/planner/homotopy/HEdge.java
@@ -23,4 +23,23 @@ public class HEdge<V, E> {
     public HNode<V> getTarget() {
         return target;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        HEdge hEdge = (HEdge) o;
+
+        if (edge != null ? !edge.equals(hEdge.edge) : hEdge.edge != null) return false;
+        if (source != null ? !source.equals(hEdge.source) : hEdge.source != null) return false;
+        if (target != null ? !target.equals(hEdge.target) : hEdge.target != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return edge != null ? edge.hashCode() : 0;
+    }
 }
