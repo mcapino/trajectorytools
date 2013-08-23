282f83249147faf0158a36446bcff8025f9fa8bb
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-08-23 16:32:13 +0200

    not drawing useless points any more

--- src/main/java/tt/discrete/vis/TrajectoryLayer.java
+++ src/main/java/tt/discrete/vis/TrajectoryLayer.java
@@ -1,15 +1,5 @@
 package tt.discrete.vis;
 
-import java.awt.Color;
-import java.util.ArrayList;
-
-import javax.vecmath.Point2d;
-import javax.vecmath.Point3d;
-
-import org.apache.log4j.Logger;
-
-import tt.discrete.Trajectory;
-import tt.vis.ProjectionTo2d;
 import cz.agents.alite.vis.element.StyledLine;
 import cz.agents.alite.vis.element.StyledPoint;
 import cz.agents.alite.vis.element.aggregation.StyledLineElements;
@@ -21,6 +11,14 @@ import cz.agents.alite.vis.layer.common.CommonLayer;
 import cz.agents.alite.vis.layer.terminal.StyledLineLayer;
 import cz.agents.alite.vis.layer.terminal.StyledPointLayer;
 import cz.agents.alite.vis.layer.toggle.KeyToggleLayer;
+import org.apache.log4j.Logger;
+import tt.discrete.Trajectory;
+import tt.vis.ProjectionTo2d;
+
+import javax.vecmath.Point2d;
+import javax.vecmath.Point3d;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class TrajectoryLayer extends CommonLayer {
 
@@ -31,15 +29,17 @@ public class TrajectoryLayer extends CommonLayer {
     }
 
     public static <X> VisLayer create(final TrajectoryProvider<X> trajectoryProvider, final ProjectionTo2d<X> projection,
-            final Color color, final int samplingInterval, final int maxTimeArg, final char toggleKey) {
+                                      final Color color, final int samplingInterval, final int maxTimeArg, final char toggleKey) {
         return create(trajectoryProvider, projection, color, samplingInterval, maxTimeArg, 4, toggleKey);
     }
 
     public static <X> VisLayer create(final TrajectoryProvider<X> trajectoryProvider, final ProjectionTo2d<X> projection,
-            final Color color, final int samplingInterval, final int maxTimeArg, final int pointSize, final char toggleKey) {
+                                      final Color color, final int samplingInterval, final int maxTimeArg, final int pointSize, final char toggleKey) {
         GroupLayer group = GroupLayer.create();
 
         group.addSubLayer(StyledPointLayer.create(new StyledPointElements() {
+
+            private Point2d previous;
 
             @Override
             public Iterable<? extends StyledPoint> getPoints() {
@@ -52,18 +52,22 @@ public class TrajectoryLayer extends CommonLayer {
                     Point2d start = projection.project(traj.get(traj.getMinTime()));
                     Point2d target = projection.project(traj.get(maxTime));
 
-                    if (start != null ) {
-                        points.add(new StyledPointImpl( new Point3d(start.x, start.y, 0), color, pointSize));
+                    if (start != null) {
+                        points.add(new StyledPointImpl(new Point3d(start.x, start.y, 0), color, pointSize));
                     }
 
                     if (target != null) {
-                        points.add(new StyledPointImpl( new Point3d(target.x, target.y, 0), color, 8));
+                        points.add(new StyledPointImpl(new Point3d(target.x, target.y, 0), color, 8));
                     }
 
                     for (int time = traj.getMinTime(); time < maxTime; time += samplingInterval) {
                         X pos = traj.get(time);
                         if (pos != null) {
                             Point2d point = projection.project(pos);
+
+                            if (previous != null && point.distance(previous) == 0)
+                                continue;
+
                             if (point != null) {
                                 points.add(new StyledPointImpl(new Point3d(point.x, point.y, 0), color, pointSize));
                             }
