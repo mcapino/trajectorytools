a4656d5781a45541222483203e0621dae46ead93
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-08-27 18:55:46 +0200

    calculation of edge penalty extracted

--- src/main/java/tt/euclidtime3i/discretization/softconstraints/PairwiseSoftConstraintWrapper.java
+++ src/main/java/tt/euclidtime3i/discretization/softconstraints/PairwiseSoftConstraintWrapper.java
@@ -2,13 +2,12 @@ package tt.euclidtime3i.discretization.softconstraints;
 
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.graph.GraphDelegator;
-
 import tt.euclid2i.Trajectory;
 import tt.euclid2i.trajectory.LinearTrajectory;
 import tt.euclidtime3i.Point;
 import tt.euclidtime3i.discretization.Straight;
 
-public class PairwiseSoftConstraintWrapper<V extends Point, E extends Straight> extends GraphDelegator<V,E>{
+public class PairwiseSoftConstraintWrapper<V extends Point, E extends Straight> extends GraphDelegator<V, E> {
 
     private Trajectory[] otherTrajs;
     private PairwiseConstraint constraint;
@@ -23,17 +22,23 @@ public class PairwiseSoftConstraintWrapper<V extends Point, E extends Straight> 
 
     @Override
     public double getEdgeWeight(E e) {
+        double cost = super.getEdgeWeight(e);
+        double penalty = calculateEdgePenalty(e);
+        return cost + penalty;
+    }
+
+    public double calculateEdgePenalty(E e) {
+        double penalty = 0;
 
         int duration = e.getEnd().getTime() - e.getStart().getTime();
         double distance = e.getStart().getPosition().distance(e.getEnd().getPosition());
         Trajectory traj = new LinearTrajectory(e.getStart().getTime(), e.getStart().getPosition(), e.getEnd().getPosition(), (int) Math.round(distance / duration), duration, super.getEdgeWeight(e));
 
-        double cost = super.getEdgeWeight(e);
         for (int i = 0; i < otherTrajs.length; i++) {
-            cost += weight * constraint.getPenalty(traj, otherTrajs[i]);
+            penalty += weight * constraint.getPenalty(traj, otherTrajs[i]);
         }
 
-        return cost;
+        return penalty;
     }
 
 }
