af3b5c3a2a1940eb09f4a6842d49d26963e79e63
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-08-28 12:22:56 +0200

    PerfectHeuristic introduced

---
+++ src/main/java/org/jgrapht/util/heuristics/PerfectHeuristic.java
@@ -0,0 +1,54 @@
+package org.jgrapht.util.heuristics;
+
+import org.jgrapht.Graph;
+import org.jgrapht.alg.AStarShortestPathSimple;
+import org.jgrapht.util.Goal;
+import org.jgrapht.util.HeuristicToGoal;
+
+import java.util.Map;
+
+public class PerfectHeuristic<S, E> implements HeuristicToGoal<S> {
+
+    private Graph<S, E> graph;
+    private Map<S, Double> distances;
+
+    public PerfectHeuristic(Graph<S, E> graph, S goal) {
+        this.graph = graph;
+        this.distances = getDistances(goal);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<S, Double> getDistances(S goal) {
+        return DijkstraShortestPath.calculateDistancesToGoal(graph, goal);
+    }
+
+    @Override
+    public double getCostToGoalEstimate(S current) {
+        Double estimate = distances.get(current);
+        return (estimate == null) ? Double.POSITIVE_INFINITY : estimate;
+    }
+
+    private static class DijkstraShortestPath<S, E> extends AStarShortestPathSimple<S, E> {
+
+        private static <S, E> Map<S, Double> calculateDistancesToGoal(Graph<S, E> graph, S goal) {
+            DijkstraShortestPath<S, E> alg = new DijkstraShortestPath<S, E>(graph, goal, new Goal<S>() {
+                @Override
+                public boolean isGoal(S current) {
+                    return false;
+                }
+            });
+
+            alg.findPath(Integer.MAX_VALUE);
+            return alg.getShortestDistances();
+        }
+
+        private DijkstraShortestPath(Graph<S, E> graph, S startVertex, Goal<S> goal) {
+            super(graph, new ZeroHeuristic<S>(), startVertex, goal);
+        }
+
+        public Map<S, Double> getShortestDistances() {
+            return shortestDistanceToVertex;
+        }
+    }
+}
