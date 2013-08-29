8516bea4d30d02ad8a301b13d277083f813d699f
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-08-29 11:52:08 +0200

    heuristics provider for MAHeuristic introduced

--- src/main/java/org/jgrapht/util/heuristics/HeuristicProvider.java
+++
@@ -1,8 +0,0 @@
-package org.jgrapht.util.heuristics;
-
-import org.jgrapht.Graph;
-import org.jgrapht.util.HeuristicToGoal;
-
-public interface HeuristicProvider<S, E> {
-    public HeuristicToGoal<S> getHeuristicToGoal(Graph<S, E> graph, S goal);
-}
