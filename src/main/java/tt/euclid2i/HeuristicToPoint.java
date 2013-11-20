ab67617769769f0a7b27b229689cdd8afbaa9889
Vojtech Letal <letal@agents.fel.cvut.cz>
2013-11-20 19:22:11 +0100

    heuristic implementation introduced
    introduced an implementation of simple heuristic pointing to single point in point2i space

---
+++ src/main/java/tt/euclid2i/HeuristicToPoint.java
@@ -0,0 +1,17 @@
+package tt.euclid2i;
+
+import org.jgrapht.util.HeuristicToGoal;
+
+public class HeuristicToPoint<P extends Point> implements HeuristicToGoal<P> {
+
+    private final P target;
+
+    public HeuristicToPoint(P target) {
+        this.target = target;
+    }
+
+    @Override
+    public double getCostToGoalEstimate(P current) {
+        return current.distance(target);
+    }
+}
