17f3821d3e7923854443ef86eba6898319782a51
Vojtech Letal <letal@agents.fel.cvut.cz>
2014-02-18 14:42:37 +0100

    naive implementation of "degree of" and "edges of"

--- src/main/java/org/jgrapht/graph/AbstractDirectedGraphWrapper.java
+++ src/main/java/org/jgrapht/graph/AbstractDirectedGraphWrapper.java
@@ -3,13 +3,14 @@ package org.jgrapht.graph;
 import org.jgrapht.DirectedGraph;
 import tt.util.NotImplementedException;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public abstract class AbstractDirectedGraphWrapper<V, E> extends AbstractGraphWrapper<V, E> implements DirectedGraph<V, E> {
 
     @Override
     public int inDegreeOf(V vertex) {
-        throw new NotImplementedException();
+        return incomingEdgesOf(vertex).size();
     }
 
     @Override
@@ -19,11 +20,19 @@ public abstract class AbstractDirectedGraphWrapper<V, E> extends AbstractGraphWr
 
     @Override
     public int outDegreeOf(V vertex) {
-        throw new NotImplementedException();
+        return outgoingEdgesOf(vertex).size();
     }
 
     @Override
     public Set<E> outgoingEdgesOf(V vertex) {
         throw new NotImplementedException();
+    }
+
+    @Override
+    public Set<E> edgesOf(V vertex) {
+        HashSet<E> edges = new HashSet<E>();
+        edges.addAll(incomingEdgesOf(vertex));
+        edges.addAll(outgoingEdgesOf(vertex));
+        return edges;
     }
 }
