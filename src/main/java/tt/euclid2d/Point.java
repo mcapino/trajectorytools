28eac8084b5b90f04f6c430bda37db4d6eefccc7
Michal Cap <cap@agents.fel.cvut.cz>
2014-03-04 17:00:26 +0100

    cleanup

--- src/main/java/tt/euclid2d/Point.java
+++ src/main/java/tt/euclid2d/Point.java
@@ -6,6 +6,7 @@ import javax.vecmath.Tuple2d;
 
 public class Point extends Point2d {
 
+	private static final long serialVersionUID = 1936317975431366847L;
     private static final Point zero = new Point(0, 0);
 
     public static Point zero() {
@@ -28,9 +29,6 @@ public class Point extends Point2d {
         super(t1);
     }
 
-    private static final long serialVersionUID = 1936317975431366847L;
-//    private static final int DECIMAL_PLACES_FOR_EQUALS_AND_HASH_CODE = 4;
-
     public static Point interpolate(Point p1, Point p2, double alpha) {
         Point result = new Point();
         ((Tuple2d) result).interpolate(p1, p2, alpha);
@@ -45,34 +43,5 @@ public class Point extends Point2d {
     public String toString() {
         return String.format("(%.2f, %.2f)", x, y);
     }
-
-//    @Override
-//    public int hashCode() {
-//    	int hash = (toFixedPoint(x, DECIMAL_PLACES_FOR_EQUALS_AND_HASH_CODE) ^ toFixedPoint(y, DECIMAL_PLACES_FOR_EQUALS_AND_HASH_CODE));
-//        return hash;
-//    }
-//
-//    @Override
-//    public boolean equals(Tuple2d t1)
-//    {
-//        try {
-//           return(toFixedPoint(this.x, DECIMAL_PLACES_FOR_EQUALS_AND_HASH_CODE) == toFixedPoint(t1.x, DECIMAL_PLACES_FOR_EQUALS_AND_HASH_CODE)
-//                  && toFixedPoint(this.y, DECIMAL_PLACES_FOR_EQUALS_AND_HASH_CODE) == toFixedPoint(t1.y, DECIMAL_PLACES_FOR_EQUALS_AND_HASH_CODE));
-//        }
-//        catch (NullPointerException e2) {return false;}
-//
-//    }
-//
-//    /**
-//     *  Converts a double value to a fixed-point representation
-//     *  @param d the original double
-//     *  @param n the number of decimal places after the radix point to be considered
-//     *  @returns a long value which approximately corresponds to d*10^n
-//     */
-//    static int toFixedPoint(double d, int n) {
-//        int value = (int) Math.round(d * Math.pow(10,n));
-//        return value;
-//    }
-
 
 }
