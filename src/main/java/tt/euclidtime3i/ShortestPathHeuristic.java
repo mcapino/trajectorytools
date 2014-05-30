9379ac5bcb6574f08cedcbb4f1a934da1a4df775
Michal Cap <cap@agents.fel.cvut.cz>
2014-05-30 16:53:40 +0200

    Renamed PerfectBasedHeuristic->ShortestPathHeuristic

--- src/main/java/tt/euclidtime3i/ShortestPathHeuristic.java
+++ src/main/java/tt/euclidtime3i/ShortestPathHeuristic.java
@@ -5,11 +5,11 @@ import org.jgrapht.util.HeuristicToGoal;
 import org.jgrapht.util.heuristics.PerfectHeuristic;
 import tt.euclid2i.Line;
 
-public class PerfectBasedHeuristic implements HeuristicToGoal<Point> {
+public class ShortestPathHeuristic implements HeuristicToGoal<Point> {
 
     private HeuristicToGoal<tt.euclid2i.Point> heuristics;
 
-    public PerfectBasedHeuristic(Graph<tt.euclid2i.Point, Line> graph, tt.euclid2i.Point target) {
+    public ShortestPathHeuristic(Graph<tt.euclid2i.Point, Line> graph, tt.euclid2i.Point target) {
         this.heuristics = new PerfectHeuristic<tt.euclid2i.Point, Line>(graph, target);
     }
 
