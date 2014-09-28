cbb53ad21d32f809b6e44a4a7e5d60cb826dafa0
Michal Cap <cap@agents.fel.cvut.cz>
2014-09-28 11:52:56 +0200

    LinearSeparationPenaltyFunction has a minPenalty parameter.

--- src/main/java/tt/euclidtime3i/discretization/softconstraints/LinearSeparationPenaltyFunction.java
+++ src/main/java/tt/euclidtime3i/discretization/softconstraints/LinearSeparationPenaltyFunction.java
@@ -3,17 +3,28 @@ package tt.euclidtime3i.discretization.softconstraints;
 public class LinearSeparationPenaltyFunction implements PenaltyFunction {
 
     private double maxPenalty;
+    private double minPenalty;
 	private double minSeparation;
-
+	
     public LinearSeparationPenaltyFunction(double maxPenalty, double minSeparation) {
+    	this(maxPenalty, minSeparation, 0);
+    }
+
+    public LinearSeparationPenaltyFunction(double maxPenalty, double minSeparation, double minPenalty) {
         super();
         this.maxPenalty = maxPenalty;
+        this.minPenalty = minPenalty;
         this.minSeparation = minSeparation;
     }
 
     @Override
     public double getPenalty(double dist, double t) {
-        return Math.max(0, maxPenalty - (maxPenalty * dist) / minSeparation);
+        if (dist > minSeparation) {
+        	return 0;
+        } else {
+        	return Math.max(minPenalty, maxPenalty - (maxPenalty * dist) / minSeparation);
+        }
+    	
     }
 
     public double getMinSeparation() {
